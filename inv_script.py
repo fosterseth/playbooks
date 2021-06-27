d318d42e5910fe3f3a1d8eb92a826a9703067206
Seth Foster <fosterseth@users.noreply.github.com>
2021-06-27 00:49:48 -0400

    Update inv_script.py

--- inv_script.py
+++ inv_script.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python
-
+from argparse import ArgumentParser
 import json
 import requests
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('--list', dest='list_instances', action='store_true', default=True,
+                        help='List instances (default: True)')
+    parser.add_argument('--host', dest='requested_host', help='Get all the variables about a specific instance')
+    return parser.parse_args()
 
 def main():
   res=requests.get('https://raw.githubusercontent.com/fosterseth/playbooks/master/inv.json')
